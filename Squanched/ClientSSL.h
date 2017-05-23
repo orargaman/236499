d1d460a792d07c38990e6bb3a6d1f8541632db11
orargaman <orargaman@gmail.com>
2017-05-23 08:30:51 +0300

    Forgoten file clientssl.h
    Signed-off-by: orargaman <orargaman@gmail.com>

---
+++ Squanched/ClientSSL.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <stdio.h>
+#include <curl/curl.h>
+#include <string>
+#include "Common.h"
+#define SKIP_HOSTNAME_VERIFICATION
+#define SKIP_PEER_VERIFICATION
+Status SendToServer(string str);
+Status getFromServer(string id, string& IV, string& key);
