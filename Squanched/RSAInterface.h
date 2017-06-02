fc05b1ac60fc8a749d3f361443f9e27dbf9c52f9
Orr <orrgoldman@campus.technion.ac.il>
2017-06-02 10:58:07 +0300

    Added RSA interface
    Added 2 classes: Rsa{En,De}cryptor.
    Interface to both is Explained in h file, but basically, you parse the xml
    file you get from the server and feed it to their init functions (either
    2 strings for encryptor or a struct for decryptor)
    
    Also, added 64Base coder, needed for the after-Parse handling.
    
    According to MSDN, you need to link to Advapi32.lib, but I linked
    with Crypt32.lib and all worked well.
    
    Note: in RSAInterface.cpp there's a usage example!

---
+++ Squanched/RSAInterface.h
@@ -0,0 +1,50 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <Windows.h>
+#include <Wincrypt.h>
+
+#include <string>
+#include <vector>
+#include "base64.h"
+using std::string;
+using std::vector;
+
+struct StringPrivateBlob
+{
+	//all in 64Base as given by xml description
+	string smod;
+	string sexp;
+	string sP;
+	string sQ;
+	string sDP;
+	string sDQ;
+	string sInverseQ;
+	string sD;
+};
+
+class RsaEncryptor
+{
+	HCRYPTPROV hCryptProv;
+	HCRYPTKEY hKey;
+public:
+	RsaEncryptor();
+	//both in 64Base, as given by xml description
+	void init_Encryptor(const string& sModulus, const string& sExp);
+	//returned buffer is alloc'd with heapAlloc.
+	PBYTE encrypt(PBYTE msg, DWORD length);
+	~RsaEncryptor();
+};
+
+class RsaDecryptor
+{
+	HCRYPTPROV hCryptProv;
+	HCRYPTKEY hKey;
+public:
+	RsaDecryptor();
+	//both in 64Base, as given by xml description
+	void init_Decryptor(const StringPrivateBlob&);
+	//returned buffer is alloc'd with heapAlloc, original msg stays as is
+	PBYTE decrypt(PBYTE msg, DWORD length = 128);
+	~RsaDecryptor();
+};
